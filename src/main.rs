8868f59c0a2ca807a5133977bfeab642128c2806
David Rett <80925809+dprdev@users.noreply.github.com>
2023-05-24 22:03:23 -0500

    Lib.rs, Tests and EV
    Start of 12.6 Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|_err| {
+        process::exit(1);
+    });
 
-    println!("Search for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read a file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
+    if let Err(_e) = minigrep::run(config) {
+        process::exit(1);
+    }
 }
